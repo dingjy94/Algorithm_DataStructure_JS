a77d16b8b76311aee34e1aa8319d165679155b89
JINGYIDING <dingjy94@gmail.com>
2019-09-26 13:27:08 -0700

    add level order to dataStructure/tree/traverseBinaryTree.js

--- dataStructure/tree/traverseBinaryTree.js
+++ dataStructure/tree/traverseBinaryTree.js
@@ -61,3 +61,25 @@ var postorderTraversal = function (root) {
 
   return result;
 };
+
+// level-order (BFS)
+var levelOrder = function (root) {
+  let result = [], queue = [];
+  if (root === null) return result;
+  queue.push(root);
+
+  while (queue.length !== 0) {
+    let level = [];
+    let count = queue.length;
+    while (count > 0) {
+      const cur = queue.shift();
+      level.push(cur.val);
+      if (cur.left) queue.push(cur.left);
+      if (cur.right) queue.push(cur.right);
+      count--;
+    }
+    result.push(level);
+  }
+
+  return result;
+};
